4174877df36b68b89747e49e9f6748f2871649f9
24f2004667 <24f2004667@ds.iitm.ac.in>
2026-05-18 12:48:28 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:3000',
+  'https://employee-analytics-2-e70p.onrender.com'
 ].filter(Boolean);
 
 app.use(
